4afe61553a9b4b7c31376a8ded0bd82ee5e62b9a
e22m4u <e22m4u@gmail.com>
2023-09-23 13:27:47 +0300

    fix: example of "waitAsync" utility

--- src/utils/wait-async.js
+++ src/utils/wait-async.js
@@ -5,7 +5,7 @@ import {InvalidArgumentError} from '@e22m4u/js-repository';
  *
  * @example
  * ```ts
- * await wait(1000); // 1sec
+ * await waitAsync(1000); // 1sec
  * ```
  *
  * @param {number} ms Milliseconds
@@ -14,7 +14,7 @@ import {InvalidArgumentError} from '@e22m4u/js-repository';
 export function waitAsync(ms) {
   if (typeof ms !== 'number')
     throw new InvalidArgumentError(
-      'The first argument of "waitAsync" must be' + ' a Number, but %v given.',
+      'The first argument of "waitAsync" must be a Number, but %v given.',
       ms,
     );
   return new Promise(r => setTimeout(() => r(), ms));
